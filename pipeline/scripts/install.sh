83897883f7212fa6f4cb891cc6f6aa6afc70b44b
Mojtaba Imani <sm.imani@gmail.com>
2023-08-13 17:34:23 +0200

    fixes the ns of argo projects

--- pipeline/scripts/install.sh
+++ pipeline/scripts/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # argocd projects
-kubectl apply --context sodeko-dta -n green-tst -f pipeline/argocd/project-green-tst.yaml
-kubectl apply --context sodeko-dta -n green-acc -f pipeline/argocd/project-green-acc.yaml
-kubectl apply --context sodeko-prd -n green-prd -f pipeline/argocd/project-green-prd.yaml
+kubectl apply --context sodeko-dta -f pipeline/argocd/project-green-tst.yaml
+kubectl apply --context sodeko-dta -f pipeline/argocd/project-green-acc.yaml
+kubectl apply --context sodeko-prd -f pipeline/argocd/project-green-prd.yaml
 
 # app of the apps
-kubectl apply --context sodeko-dta -n green-tst -f pipeline/argocd/app-of-apps-tst.yaml
-kubectl apply --context sodeko-dta -n green-acc -f pipeline/argocd/app-of-apps-acc.yaml
-kubectl apply --context sodeko-prd -n green-prd -f pipeline/argocd/app-of-apps-prd.yaml
+kubectl apply --context sodeko-dta -f pipeline/argocd/app-of-apps-green-tst.yaml
+kubectl apply --context sodeko-dta -f pipeline/argocd/app-of-apps-green-acc.yaml
+kubectl apply --context sodeko-prd -f pipeline/argocd/app-of-apps-green-prd.yaml
